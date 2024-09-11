63b2f7220fdb4063af6ab447f700e6c851d14da5
Mouadh Khlifi <mouadh.khlifi@codecentric.de>
2024-09-11 14:40:30 +0200

    Add add()

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,19 +1,9 @@
-describe("A Test", () => {
-  it("fails", () => {
-    expect(false as boolean).toEqual(true as boolean);
-  });
+function add(number:string):number{
+  return 0;
+}
 
-    it("succeeds", () => {
-    expect(true as boolean).toEqual(true as boolean);
-  });
-
-});
 
 describe("Should return 0 when input empty", () => {
-  it("fails", () => {
-    expect(false as boolean).toEqual(true as boolean);
-  });
-
     it("succeeds", () => {
     expect(add("")).toEqual(0);
   });
