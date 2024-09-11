2e7eceb669bbca3fbe32c51b8d18591ae8d0d596
erik.petzold <erik.petzold@codecentric.de>
2024-09-11 14:45:01 +0200

    erik

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,12 +1,21 @@
-function add(number:string):number{
-  return 0;
+function add(number: string): number {
+    return 0;
+    if (number === "1") {
+      return 1;
+    }
 }
 
 
 describe("Should return 0 when input empty", () => {
     it("succeeds", () => {
-    expect(add("")).toEqual(0);
-  });
+        expect(add("")).toEqual(0);
+    });
+});
 
+
+describe("Should return 1 when input 1", () => {
+    it("succeeds", () => {
+        expect(add("1")).toEqual(1);
+    });
 });
 
