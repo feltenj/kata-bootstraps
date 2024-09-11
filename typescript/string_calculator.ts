5af80155f4450c5fee98764e82350acf74317061
Frederik Kischewski <frederik.kischewski@codecentric.de>
2024-09-11 14:52:04 +0200

    Number to string

--- typescript/string_calculator.ts
+++ typescript/string_calculator.ts
@@ -1,12 +1,15 @@
-export function add(number: string): string {
-    if(number === ""){
+export function add(inputString: string): string {
+    if(inputString === ""){
         return "0"
     }
-    let res = number.split(",").map(it => {
+    let res = inputString.split(",").map(it => {
         let number = parseFloat(it);
         return number
     })
-    return "";
+
+    const resultAsNumber = res.reduce((sum, current) => sum + current, 0);
+
+    return resultAsNumber.toString();
 }
 
 
