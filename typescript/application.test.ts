5e332149b41d21cd07c6070629d423d9826313e1
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-09-15 16:56:50 +0200

    add many numbers solution (without error handling)

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,13 +1,22 @@
 function add(number: string): string {
+
     if (number === "") {
         return "0";
-    }
-    if (number.includes(",")) {
-        let splittedEquation = number.split(",");
-        let item1 = Number(splittedEquation[0]);
-        let item2 = Number(splittedEquation[1]);
 
-        return String(item1 + item2);
+    } else if (number.includes(",")) {
+
+        let splittedEquation = number.split(",");
+
+        let numberArray = splittedEquation.map(function (number) {
+            return Number(number)
+        });
+
+        let sum = 0;
+        for (let i = 0; i < numberArray.length; i++) {
+            sum += numberArray[i]
+        }
+
+        return String(sum);
 
     } else {
         return number;
@@ -30,9 +39,14 @@ describe("Should return a number when input only one number", () => {
 
 describe("Comma separated input should add the separated values", () => {
     it("succeeds", () => {
-        expect(add("5.1,90")).toEqual("95.1");
+        expect(add("5,90")).toEqual("95");
     });
 });
 
+describe("Check add method with unknown number of arguments", () => {
+    it("succeeds", () => {
+        expect(add("5,90,5,5")).toEqual("105");
+    });
+});
 
 
