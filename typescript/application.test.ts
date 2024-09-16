19c18bb5e7a29e0d4433aae7a5e101abc4414bc0
erik.petzold <erik.petzold@codecentric.de>
2024-09-16 13:51:34 +0200

    wip

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -10,29 +10,6 @@ function add(number: string): string {
     return String(Math.round(sum * 10)/10);
 }
 
-describe("Should return 0 when input empty", () => {
-    it("succeeds", () => {
-        expect(add("")).toEqual("0");
-    });
-});
-
-describe("Should return 1 when input 1", () => {
-    it("succeeds", () => {
-        expect(add("1")).toEqual("1");
-    });
-});
-
-describe("Comma separated input should add the separated values", () => {
-    it("succeeds", () => {
-        expect(add("1,2")).toEqual("3");
-    });
-});
-
-describe("Should return 2 when input 2", () => {
-    it("succeeds", () => {
-        expect(add("2")).toEqual("2");
-    });
-});
 
 describe("Comma separated input should add the 3 separated values", () => {
     it("succeeds", () => {
@@ -46,6 +23,20 @@ describe("Add should work on floats", () => {
     });
 });
 
-describe("When input is ", () => {
+describe("When input is", () => {
+    it("empty, sum should return 0", () => {
+        expect(add("")).toEqual("0");
+    });
 
+    it("1, sum should return 1", () => {
+        expect(add("1")).toEqual("1");
+    });
+
+    it("2, sum should return 2", () => {
+        expect(add("2")).toEqual("2");
+    });
+
+    it("comma separated values then add should add the values", () => {
+        expect(add("1,2")).toEqual("3");
+    });
 });
