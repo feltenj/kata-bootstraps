5ba39d884ff27a3011c01f8aa0208c1b47d739a3
elinaonchul <elina.onchul@codecentric.de>
2024-09-16 13:34:04 +0200

    nex

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,14 +1,14 @@
 function add(number: string): number {
-    if (number === "1") {
-      return 1;
-    } else if(number === "") {
-        return 0;
-    }
+  if (number === "1") {
+    return 1;
+  } else if (number === "") {
+    return 0;
+  }
 
-    let splittedEquation = number.split(",").reduce((sum, current)=>{
-        return Number(sum)+Number(current);
-    },0);
-    /*let length = splittedEquation.length;
+  let sum = number.split(",").reduce((sum, current) => {
+    return Number(sum) + Number(current);
+  }, 0);
+  /*let length = splittedEquation.length;
     if (length === 1) {
         return Number(splittedEquation[0]);
     }
@@ -16,37 +16,41 @@ function add(number: string): number {
     for (let i = 0; i < length; i++) {
         sum += Number(splittedEquation[i])
     }*/
-    return splittedEquation;
+  return sum;
 }
 
-
 describe("Should return 0 when input empty", () => {
-    it("succeeds", () => {
-        expect(add("")).toEqual(0);
-    });
+  it("succeeds", () => {
+    expect(add("")).toEqual(0);
+  });
 });
 
-
 describe("Should return 1 when input 1", () => {
-    it("succeeds", () => {
-        expect(add("1")).toEqual(1);
-    });
+  it("succeeds", () => {
+    expect(add("1")).toEqual(1);
+  });
 });
 
 describe("Comma separated input should add the separated values", () => {
-    it("succeeds", () => {
-        expect(add("1,2")).toEqual(3);
-    });
+  it("succeeds", () => {
+    expect(add("1,2")).toEqual(3);
+  });
 });
 
 describe("Should return 2 when input 2", () => {
-    it("succeeds", () => {
-        expect(add("2")).toEqual(2);
-    });
+  it("succeeds", () => {
+    expect(add("2")).toEqual(2);
+  });
 });
 
 describe("Comma separated input should add the 3 separated values", () => {
-    it("succeeds", () => {
-        expect(add("1,2,3")).toEqual(6);
-    });
+  it("succeeds", () => {
+    expect(add("1,2,3")).toEqual(6);
+  });
+});
+
+describe("Add should work on floats", () => {
+  it("succeeds", () => {
+    expect(add("1.1,2.2")).toEqual(3.3);
+  });
 });
