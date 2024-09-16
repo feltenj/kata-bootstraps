11b5aff796d4a9d4c4e3a479980ece800ea9b90d
elinaonchul <elina.onchul@codecentric.de>
2024-09-16 14:04:12 +0200

    next

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,38 +1,40 @@
 function add(number: string): string {
-   if (number === "") {
-        return "0";
-    }
+  if (number === "") {
+    return "0";
+  }
 
-    let sum = number.split(",").reduce((sum, current) => {
-        return Number(sum) + Number(current);
-    }, 0);
+  let sum = number.split(/[\n|,]/).reduce((sum, current) => {
+    return Number(sum) + Number(current);
+  }, 0);
 
-    return String(Math.round(sum * 10)/10);
+  return String(Math.round(sum * 10) / 10);
 }
 
 describe("When input is", () => {
-    it("empty, sum should return 0", () => {
-        expect(add("")).toEqual("0");
-    });
+  it("empty, sum should return 0", () => {
+    expect(add("")).toEqual("0");
+  });
 
-    it("1, sum should return 1", () => {
-        expect(add("1")).toEqual("1");
-    });
+  it("1, sum should return 1", () => {
+    expect(add("1")).toEqual("1");
+  });
 
-    it("2, sum should return 2", () => {
-        expect(add("2")).toEqual("2");
-    });
+  it("2, sum should return 2", () => {
+    expect(add("2")).toEqual("2");
+  });
 
-    it("2 comma separated values then add should add the values", () => {
-        expect(add("1,2")).toEqual("3");
-    });
-    it("more then 2 comma separated values then return sum", () => {
-        expect(add("1,2,3")).toEqual("6");
-    });
-    it("float then return sum", () => {
-        expect(add("1.1,2.2")).toEqual("3.3");
-    });
-    it("seperated with \n then it should work as a seperator like a comma", () => {
-        expect(add("1\n2,3")).toEqual("6");
-    });
+  it("2 comma separated values then add should add the values", () => {
+    expect(add("1,2")).toEqual("3");
+  });
+  it("more then 2 comma separated values then return sum", () => {
+    expect(add("1,2,3")).toEqual("6");
+  });
+  it("float then return sum", () => {
+    expect(add("1.1,2.2")).toEqual("3.3");
+  });
+  it("seperated with \n then it should work as a seperator like a comma", () => {
+    expect(add("1\n2,3")).toEqual("6");
+  });
 });
+
+describe("Is inputValid is", () => {});
