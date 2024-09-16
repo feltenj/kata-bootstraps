4b627b89192928c0d37152b42fdd89766a80e13c
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-09-16 13:45:50 +0200

    refactor

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,22 +1,13 @@
 function add(number: string): string {
-    if (number === "1") {
-        return "1";
-    } else if (number === "") {
+   if (number === "") {
         return "0";
     }
 
     let sum = number.split(",").reduce((sum, current) => {
         return Number(sum) + Number(current);
     }, 0);
-    /*let length = splittedEquation.length;
-      if (length === 1) {
-          return Number(splittedEquation[0]);
-      }
-      let sum = 0;
-      for (let i = 0; i < length; i++) {
-          sum += Number(splittedEquation[i])
-      }*/
-    return sum.toFixed(1);
+
+    return String(Math.round(sum * 10)/10);
 }
 
 describe("Should return 0 when input empty", () => {
@@ -53,4 +44,8 @@ describe("Add should work on floats", () => {
     it("succeeds", () => {
         expect(add("1.1,2.2")).toEqual("3.3");
     });
+});
+
+describe("When input is ", () => {
+
 });
