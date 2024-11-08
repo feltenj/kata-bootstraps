d133d5a0d39bf6d5e367d207884c1517e0465d0e
janinefelten <janine.felten92@gmail.com>
2024-11-08 10:30:23 +0100

    🚧 mob next

--- kotlin/src/test/kotlin/ApplicationSpec.kt
+++ kotlin/src/test/kotlin/ApplicationSpec.kt
@@ -6,58 +6,69 @@ import io.kotest.matchers.shouldBe
 class ApplicationSpec() :
     DescribeSpec({
         describe("Game of Life") {
-                    data class TestCase (
-                        val description: String,
-                        val livingNeighbours: LivingNeighbours,
-                        val expectedCellState: CellState
-                    ): WithDataTestName {
-                        override fun dataTestName() = description
-                    }
+            data class TestCase(
+                val description: String,
+                val livingNeighbours: LivingNeighbours,
+                val expectedCellState: CellState,
+                val currentCellState: CellState,
+            ) : WithDataTestName {
+                override fun dataTestName() = description
 
-      withData(
-        TestCase(
-          "underpopulation: a living cell with 0 living neighbours dies in the next generation",
-          LivingNeighbours(0),
-          CellState.DEAD
-        ),
-        TestCase(
-          "underpopulation: a living cell with 1 living neighbours dies in the next generation",
-          LivingNeighbours(1),
-          CellState.DEAD
-        ),
-        TestCase(
-          "survival: a living cell with 2 living neighbours survives to the next generation",
-          LivingNeighbours(2),
-          CellState.ALIVE
-        ),
-        TestCase(
-          "survival: a living cell with 3 living neighbours survives to the next generation",
-          LivingNeighbours(3),
-          CellState.ALIVE
-        ),
-        TestCase(
-          "overcrowding: a living cell with 4 living neighbours dies in the next generation",
-          LivingNeighbours(4),
-          CellState.DEAD
-        )
-      ) { (_, livingNeighbours, expectedCellState) ->
-        val currentCellState = CellState.ALIVE
+                constructor(
+                    description: String,
+                    livingNeighbours: LivingNeighbours,
+                    expectedCellState: CellState,
+                ) : this(description, livingNeighbours, expectedCellState, CellState.ALIVE)
+            }
 
-        val nextCellState = nextGeneration(currentCellState, livingNeighbours)
+            withData(
+                TestCase(
+                    description = "underpopulation: a living cell with 0 living neighbours dies in the next generation",
+                    livingNeighbours = LivingNeighbours(0),
+                    expectedCellState = CellState.DEAD
+                ),
+                TestCase(
+                    description = "underpopulation: a living cell with 1 living neighbours dies in the next generation",
+                    livingNeighbours = LivingNeighbours(1),
+                    expectedCellState = CellState.DEAD
+                ),
+                TestCase(
+                    description = "survival: a living cell with 2 living neighbours survives to the next generation",
+                    livingNeighbours = LivingNeighbours(2),
+                    expectedCellState = CellState.ALIVE
+                ),
+                TestCase(
+                    description = "survival: a living cell with 3 living neighbours survives to the next generation",
+                    livingNeighbours = LivingNeighbours(3),
+                    expectedCellState = CellState.ALIVE
+                ),
+                TestCase(
+                    description = "overcrowding: a living cell with 4 living neighbours dies in the next generation",
+                    livingNeighbours = LivingNeighbours(4),
+                    expectedCellState = CellState.DEAD
+                ),
+                TestCase(
+                    description = "staying dead: a dead cell with 2 living neighbours stays dead in the next generation",
+                    livingNeighbours = LivingNeighbours(2),
+                    expectedCellState = CellState.DEAD,
+                    currentCellState = CellState.DEAD
+                ),
+            ) { (_, livingNeighbours, expectedCellState, currentCellState) ->
+                val nextCellState = nextGeneration(currentCellState, livingNeighbours)
 
-        nextCellState shouldBe expectedCellState
-      }
-    }
-  })
+                nextCellState shouldBe expectedCellState
+            }
+        }
+    })
 
 fun nextGeneration(currentCellState: CellState, livingNeighbours: LivingNeighbours): CellState {
-  if (livingNeighbours.value == 2 || livingNeighbours.value == 3) return CellState.ALIVE
-  return CellState.DEAD
+    if (livingNeighbours.value == 2 || livingNeighbours.value == 3) return currentCellState
+    return CellState.DEAD
 }
 
 enum class CellState {
-  ALIVE,
-  DEAD
+    ALIVE,
+    DEAD
 }
 
 @JvmInline
