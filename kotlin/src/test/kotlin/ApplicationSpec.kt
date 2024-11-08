e06f04d9593b6070bc4e60576b134f5f6779740b
Marco Emrich <marco.emrich@codecentric.de>
2024-11-08 09:31:29 +0100

    remove failing test

--- kotlin/src/test/kotlin/ApplicationSpec.kt
+++ kotlin/src/test/kotlin/ApplicationSpec.kt
@@ -3,7 +3,5 @@ import io.kotest.matchers.shouldBe
 
 class ApplicationSpec :
         StringSpec({
-            "it should fail" { true shouldBe false }
-
-            "it should succeed" { true shouldBe true }
+            "todo" { true shouldBe true }
         })
