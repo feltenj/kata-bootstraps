225d1a60fe689161a1fed69630ea2efb968100be
Thomas Traude <thomas.traude@codecentric.de>
2024-11-08 10:02:55 +0100

    WIP

--- kotlin/src/test/kotlin/ApplicationSpec.kt
+++ kotlin/src/test/kotlin/ApplicationSpec.kt
@@ -1,19 +1,30 @@
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.datatest.WithDataTestName
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
-class ApplicationSpec :
+class ApplicationSpec() :
     DescribeSpec({
         describe("Game of Life") {
-            describe("a living cell with 0 living neighbours dies in the next generation") {
-                withData(LivingNeighbours(0), LivingNeighbours(1)) { livingNeighbours ->
+                    data class TestCase (
+                        val description: String,
+                        val livingNeighbours: LivingNeighbours,
+                        val expectedCellState: CellState
+                    ): WithDataTestName {
+                        override fun dataTestName(): String {
+                           return description
+                        }
+                    }
+
+                withData(TestCase("a living cell with 0 living neighbours dies in the next generation", LivingNeighbours(0), CellState.DEAD)                    ,
+                    TestCase("a living cell with 1 living neighbours dies in the next generation", LivingNeighbours(1), CellState.DEAD)
+                ) { (desc, livingNeighbours, expectedCellState) ->
                     val currentCellState = CellState.ALIVE
 
                     val nextCellState = nextGeneration(currentCellState, livingNeighbours)
 
-                    nextCellState shouldBe CellState.DEAD
+                    nextCellState shouldBe expectedCellState
                 }
-            }
 
             it("a living cell with 2 living neighbours survives in the next generation") {
                 val livingNeighbours = LivingNeighbours(2)
